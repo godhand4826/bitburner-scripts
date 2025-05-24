e20bc8e53bd72f06ab3e0b7c9ffb738f87b32afb
eric_huang <godhand4826@gmail.com>
2025-05-24 14:21:32 +0800

    feat(alias): add alias.ts

---
+++ src/alias.ts
@@ -0,0 +1,15 @@
+import { NS } from "@ns";
+import { run } from "./lib/terminal";
+
+export async function main(ns: NS): Promise<void> {
+    [
+        ["m", "home; ./main.js; clear"],
+        ["h", "home"],
+        ["bl", "buy -l"],
+        ["ba", "buy -a"],
+        ["ssh", "./ssh.js"],
+        ["pkill", "./pkill.js"],
+    ].forEach(([name, command]) => run(`alias ${name}='${command}'`))
+
+    ns.toast("Aliases set up successfully.");
+}

fc2d812b923c5176e2087e61502b20d96684c979
eric_huang <godhand4826@gmail.com>
2025-06-05 09:44:13 +0800

    feat(backdoor): skip w0r1d_d43m0n by default

--- src/backdoor.ts
+++ src/backdoor.ts
@@ -4,12 +4,18 @@ import { isHackable, requiredHackingSkill } from './lib/hack.js'
 import { backdoor } from './lib/backdoor.js'
 
 export async function main(ns: NS): Promise<void> {
+    const includeWorldDaemon = (ns.args[0] ?? false) as boolean
+
     ns.disableLog('ALL')
     ns.enableLog('singularity.installBackdoor')
 
     while (list(ns).some(host => !ns.getServer(host).backdoorInstalled)) {
         const hosts = list(ns, { onlyNuked: true })
-            .filter(host => !ns.getServer(host).backdoorInstalled && isHackable(ns, host))
+            .filter(host =>
+                !ns.getServer(host).backdoorInstalled &&
+                isHackable(ns, host) &&
+                (includeWorldDaemon || host != 'w0r1d_d43m0n')
+            )
             .sort((h1, h2) => requiredHackingSkill(ns, h1) - requiredHackingSkill(ns, h2))
 
         for (const host of hosts) {

e21d08c14127ef96ef480e5acaf92a30bc7c78a4
eric_huang <godhand4826@gmail.com>
2025-06-09 09:02:35 +0800

    enhance(stock): automatically balance cash and stock

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NS } from '@ns'
 import { b, m, t } from './lib/const'
 import { pkill } from './lib/remote'
-import { run as runStock } from './stock'
 import { run as runHacknet } from './hacknet'
 import { run as runServer } from './server'
 import { run as runGang } from './gang'
@@ -21,7 +20,7 @@ export async function main(ns: NS): Promise<void> {
   runHacknet(ns, 30 * m, 0, 0)
   ns.run('go.js')
   runFaction(ns, 3)
-  runStock(ns, 250 * m)
+  ns.run('stock.js')
   ns.run('sleeve.js')
   runServer(ns, 1 * t, 200 * b)
   ns.run('backdoor.js')
--- src/stock.ts
+++ src/stock.ts
@@ -1,17 +1,8 @@
 import { NS } from '@ns';
 import { purchaseStockAPIs, closeAllPosition, isFullyPurchasedStockAPIs, autoClosePositions, autoCreatePositions, getTotalPosition } from './lib/stock'
 import { disableLogs } from './lib/log';
-import { formatTime, now } from './lib/time';
-import { b } from './lib/const';
-import { getBudget } from './lib/money';
-
-export function run(ns: NS, preservedMoney = 250_000_000) {
-    ns.run('stock.js', 1, preservedMoney)
-}
 
 export async function main(ns: NS): Promise<void> {
-    const preservedMoney = (ns.args[0] ?? 250_000_000) as number
-
     disableLogs(ns, 'sleep', 'getServerMoneyAvailable')
 
     while (!isFullyPurchasedStockAPIs(ns)) {
@@ -22,23 +13,13 @@ export async function main(ns: NS): Promise<void> {
 
     ns.atExit(() => closeAllPosition(ns))
 
-    const releaseInterval = 60 * 1000;
-    const releaseTime = 5 * 1000;
-    let nextReleaseAt = now();
     for (; ;) {
-        // release money awhile for spend
-        if (now() >= nextReleaseAt && getTotalPosition(ns) > 100 * b && getBudget(ns) < 100 * b) {
-            ns.toast(`release all position for ${ns.tFormat(releaseTime)}`, 'info', releaseTime + 2000)
-            closeAllPosition(ns)
-            nextReleaseAt = now() + releaseInterval
-            await ns.asleep(releaseTime)
-            ns.toast(`next position release in ${ns.tFormat(releaseInterval)} at ${formatTime(nextReleaseAt)}`, 'info', nextReleaseAt - now() + 2000)
-        }
-
         autoClosePositions(ns)
-        autoCreatePositions(ns, preservedMoney)
+
+        // set preserved money to total position will automatically
+        // balance cash and stock over time
+        autoCreatePositions(ns, getTotalPosition(ns))
 
         await ns.stock.nextUpdate()
     }
 }
-

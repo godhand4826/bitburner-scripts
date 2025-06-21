06b7ea0d6fda6389de7694b91f17f0e078ecb50d
eric_huang <godhand4826@gmail.com>
2025-06-22 03:18:39 +0800

    chore(nuke): also open purchase server ports

--- src/nuke.ts
+++ src/nuke.ts
@@ -3,8 +3,11 @@ import { list } from './lib/host';
 import { nuke } from './lib/nuke';
 
 export async function main(ns: NS): Promise<void> {
-    while (list(ns, { onlyNuked: true }).length < list(ns).length) {
-        list(ns).forEach(host => nuke(ns, host))
+    while (
+        list(ns, { onlyNuked: true, includeHome: true, includePurchased: true }).length <
+        list(ns, { onlyNuked: false, includeHome: true, includePurchased: true }).length
+    ) {
+        list(ns, { includeHome: true, includePurchased: true }).forEach(host => nuke(ns, host))
 
         await ns.sleep(2000)
     }

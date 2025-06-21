96839232b4bac08b8bf90844aa5a1732cc5d921c
eric_huang <godhand4826@gmail.com>
2025-06-22 03:44:57 +0800

    feat(focus): add enableGrafting flags

--- src/focus.ts
+++ src/focus.ts
@@ -3,10 +3,12 @@ import { disableLogs } from './lib/log';
 import { autoSetAction } from './lib/focus';
 
 export async function main(ns: NS): Promise<void> {
+    const enableGrafting = (ns.args[0] ?? false) as boolean
+
     disableLogs(ns, 'sleep', 'getServerMoneyAvailable')
 
     for (; ;) {
-        await autoSetAction(ns)
+        await autoSetAction(ns, enableGrafting)
 
         await ns.sleep(2000)
     }

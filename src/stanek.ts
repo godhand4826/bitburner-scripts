3b4be0b46d2cae58397a530cf8e2060b07f761fb
eric_huang <godhand4826@gmail.com>
2025-06-22 11:37:01 +0800

    feat(stanek): support bigger gift board

--- src/stanek.ts
+++ src/stanek.ts
@@ -7,20 +7,33 @@ export async function main(ns: NS): Promise<void> {
     const Y = ns.stanek.giftHeight()
 
     if (X == 6 && Y == 5) {
-        ns.stanek.placeFragment(2, 1, 1, 1)
         ns.stanek.placeFragment(4, 2, 1, 0)
+        ns.stanek.placeFragment(2, 1, 1, 1)
+        ns.stanek.placeFragment(2, 3, 2, 5)
         ns.stanek.placeFragment(0, 1, 1, 6)
         ns.stanek.placeFragment(0, 0, 2, 7)
-        ns.stanek.placeFragment(2, 3, 2, 5)
-        ns.stanek.placeFragment(4, 0, 0, 21)
         ns.stanek.placeFragment(1, 1, 1, 20)
+        ns.stanek.placeFragment(4, 0, 0, 21)
+    } else if (X == 6 && Y == 6) {
+        ns.stanek.placeFragment(0, 3, 0, 0)
+        ns.stanek.placeFragment(0, 0, 1, 1)
+        ns.stanek.placeFragment(1, 1, 2, 5)
+        ns.stanek.placeFragment(0, 5, 0, 6)
+        ns.stanek.placeFragment(2, 0, 2, 7)
+        ns.stanek.placeFragment(5, 0, 1, 20)
+        ns.stanek.placeFragment(4, 4, 0, 21)
+        ns.stanek.placeFragment(2, 2, 0, 105)
     } else {
         throw new Error(`unexpected stanek gift size [X, Y] ${X}, ${Y}`)
     }
 
-
     for (; ;) {
         for (const f of ns.stanek.activeFragments()) {
+            // skip booster fragments
+            if (f.id >= 100) {
+                continue
+            }
+
             await ns.stanek.chargeFragment(f.x, f.y)
         }
     }

1aa52cb8b3169f2a0409db309b556a2d9c0b5f32
eric_huang <godhand4826@gmail.com>
2025-06-29 08:16:26 +0800

    feat(stank): support bigger gift board

--- src/stanek.ts
+++ src/stanek.ts
@@ -23,6 +23,17 @@ export async function main(ns: NS): Promise<void> {
         ns.stanek.placeFragment(5, 0, 1, 20)
         ns.stanek.placeFragment(4, 4, 0, 21)
         ns.stanek.placeFragment(2, 2, 0, 105)
+    } else if (X == 7 && Y == 6) {
+        ns.stanek.placeFragment(4, 0, 1, 0)
+        ns.stanek.placeFragment(2, 3, 3, 5)
+        ns.stanek.placeFragment(1, 2, 1, 6)
+        ns.stanek.placeFragment(1, 0, 2, 7)
+        ns.stanek.placeFragment(0, 2, 1, 20)
+        ns.stanek.placeFragment(3, 2, 0, 21)
+        ns.stanek.placeFragment(5, 3, 3, 25)
+        ns.stanek.placeFragment(5, 0, 3, 28)
+        ns.stanek.placeFragment(0, 0, 1, 104)
+        ns.stanek.placeFragment(3, 3, 0, 105)
     } else {
         throw new Error(`unexpected stanek gift size [X, Y] ${X}, ${Y}`)
     }
